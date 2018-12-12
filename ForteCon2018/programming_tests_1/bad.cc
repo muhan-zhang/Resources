3bb9b22e2b6628467fe8cae171121f719dd2e2ca
Jeff Schriber <jbschri@emory.edu>
2018-12-12 13:19:58 -0500

    Re-add bad.cc

--- ForteCon2018/programming_tests_1/bad.cc
+++ ForteCon2018/programming_tests_1/bad.cc
@@ -1,3 +1,4 @@
+#include <complex>
 #include <iomanip>
 #include <sstream>
 #include <stdlib.h>
@@ -8,14 +9,11 @@
 #include "psi4/libpsio/psio.hpp"
 
 #include "../helpers/printing.h"
-#include "aci.h"
+#include "tdaci.h"
 
-namespace psi {
-namespace forte {
+namespace psi { namespace forte {
 
-ACI::ACI(SharedWavefunction ref_wfn, Options& options, std::shared_ptr<ForteIntegrals> ints,
-         std::shared_ptr<MOSpaceInfo> mo_space_info)
-    : Wavefunction(options), ints_(ints), mo_space_info_(mo_space_info) {
+ACI::ACI(SharedWavefunction ref_wfn, Options& options, std::shared_ptr<ForteIntegrals> ints, std::shared_ptr<MOSpaceInfo> mo_space_info) : Wavefunction(options), ints_(ints), mo_space_info_(mo_space_info) {
     // Copy the wavefunction information
     shallow_copy(ref_wfn);
     wfn_ = ref_wfn;
@@ -23,29 +21,36 @@ ACI::ACI(SharedWavefunction ref_wfn, Options& options, std::shared_ptr<ForteInte
 
 ACI::~ACI() {}
 
-double ACI::compute_overlap(std::vector<Determinant>& wfn1, std::vector<double>& coeffs1,
-                            std::vector<Determinant>& wfn2, std::vector<double>& coeffs2) {
+// Francesco, shield your eyes
+double ACI::compute_overlap( vector<Determinant> &wfn1, vector<double> &coeffs1, std::vector<Determinant> &wfn2, std::vector<double> &coeffs2 ) {
 
-    double overlap = 0.0;
 
-    size_t maxB = wfn2.size();
 
-    for (size_t A = 0, maxA = wfn1.size(); A < maxA; ++A) {
-        const Determinant& detA = wfn1[A];
-        const double Ca = coeffs1[A];
-        for (size_t B = 0; B < maxB; ++B) {
+    double overlap, Ca, Cb;
+    
+    overlap = 0.0;
+    for( size_t A = 0; A < wfn1.size(); ++A )
+    {   
+      for( size_t B = 0; B < wfn2.size(); ++B ) {
+        Determinant detA = wfn1[A]; 
+        Ca = coeffs1[A];
 
-            const Determinant& detB = wfn2[B];
+        Determinant detB = wfn2[B]; 
+        Cb = coeffs2[B];
 
-            if (detA == detB) {
-                const double Cb = coeffs2[B];
-                overlap += Ca * Cb;
-                break; // Stop looping over B
-            }
+        if( detA == detB ){
+           overlap += Ca*Cb; 
+
         }
+
+      }  
     }
 
     return overlap;
 }
-}
-}
+
+
+    
+
+
+}  }

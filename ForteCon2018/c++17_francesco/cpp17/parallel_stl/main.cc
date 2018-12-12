8c6fc7d9ee00688f2f5c8a4fbd47f16170da86aa
Francesco Evangelista <francesco.evangelista@gmail.com>
2018-12-11 21:22:05 -0500

    Add example

---
+++ ForteCon2018/c++17_francesco/cpp17/parallel_stl/main.cc
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+int main() {
+  std::vector<double> v(10'000'007, 0.5);
+
+  {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    double result = std::accumulate(v.begin(), v.end(), 0.0);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> ms = t2 - t1;
+    std::cout << std::fixed << "std::accumulate result " << result << " took "
+              << ms.count() << " ms\n";
+  }
+
+  // when the parallel STL becomes available you will be able to call
+  //
+  //  #include <execution>
+  // std::reduce(std::execution::par, v.begin(), v.end());
+  //
+  //  {
+  //    auto t1 = std::chrono::high_resolution_clock::now();
+  //    double result = std::reduce(std::execution::par, v.begin(), v.end());
+  //    auto t2 = std::chrono::high_resolution_clock::now();
+  //    std::chrono::duration<double, std::milli> ms = t2 - t1;
+  //    std::cout << "std::reduce result " << result << " took " << ms.count()
+  //              << " ms\n";
+  //  }
+}
